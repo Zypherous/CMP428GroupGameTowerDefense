0e3c353514b8f6b51776364b45e7e34c39485747
BryanAlbertoMk20 <bryanalberto1@live.com>
2021-12-02 22:03:53 -0500

    GameF21 files
    Files that are needed make the game function

---
+++ src/GameF21.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+
+public class GameF21 extends GameBase{
+	
+	public void initialize() {
+		
+		
+	}	
+	
+	
+	public void inGameLoop(){
+		
+		
+		
+	}	
+	
+	public void paint(Graphics pen){
+		
+		
+		
+		
+	}	
+	
+	
+	
+}
